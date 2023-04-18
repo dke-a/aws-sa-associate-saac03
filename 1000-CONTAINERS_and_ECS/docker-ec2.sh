1bc6d415280962cec36d3425fb2b7c4bc648c79f
dke-a <angeles.don@yahoo.com>
2023-04-17 22:27:57 -0400

    modify docker yaml, add bootstrap yaml and user-data for Wordpress bootstrapping demo

--- 1000-CONTAINERS_and_ECS/docker-ec2.sh
+++ 1000-CONTAINERS_and_ECS/docker-ec2.sh
@@ -1,19 +1,27 @@
 # Install Docker Engine on EC2 Instance
 sudo amazon-linux-extras install docker
 sudo service docker start
-sudo usermod -a -G docker ec2-user
+docker ps
+#will cause an error
 
-LOGOUT and login
+sudo usermod -a -G docker ec2-user
+#add user to a group that can interact with docker engine
+
+#LOGOUT and login
 
 sudo su - ec2-user
+#login as ec2-user
 
 # Build Docker Image
 cd container
 docker build -t containerofcats .
+# build container image. gives location of docker file. dot at the end gives working directory
 docker images --filter reference=containerofcats
+
 
 # Run Container from Image
 docker run -t -i -p 80:80 containerofcats
+#map port 80 on the container with port 80 of instance
 
 # Upload Container to Dockerhub (optional)
 docker login --username=YOUR_USER
