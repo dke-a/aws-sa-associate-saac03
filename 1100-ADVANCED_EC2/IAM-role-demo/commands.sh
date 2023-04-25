5fd5555a35db66530a22f0d42abcc2a4422d8ec2
dke-a <angeles.don@yahoo.com>
2023-04-24 22:58:33 -0400

    add files for ec2 logging CW demo, R53 failover demo, splitting wordpress demo resources

--- 1100-ADVANCED_EC2/IAM-role-demo/commands.sh
+++ 1100-ADVANCED_EC2/IAM-role-demo/commands.sh
@@ -3,3 +3,7 @@ aws s3 ls
 curl http://169.254.169.254/latest/meta-data/iam/security-credentials/
 
 curl http://169.254.169.254/latest/meta-data/iam/security-credentials/A4LInstanceRole
+
+# code to list all active resources in aws account  using aws cli 
+
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Type:InstanceType,State:State.Name,PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress}' --output table
