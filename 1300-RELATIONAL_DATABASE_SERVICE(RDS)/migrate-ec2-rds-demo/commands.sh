4b8d8e71b3bcc2a2b07d8dcb3f04eee0903a31bc
dke-a <angeles.don@yahoo.com>
2023-04-29 14:48:23 -0400

    add migrate ec2 and multiaz snapshot demos files.

--- 1300-RELATIONAL_DATABASE_SERVICE(RDS)/migrate-ec2-rds-demo/commands.sh
+++ 1300-RELATIONAL_DATABASE_SERVICE(RDS)/migrate-ec2-rds-demo/commands.sh
@@ -1,9 +1,9 @@
 # Backup of Source Database
-mysqldump -h PRIVATEIPOFMARIADBINSTANCE -u a4lwordpress -p a4lwordpress > a4lwordpress.sql
-
+mysqldump -h 10.16.60.213 -u a4lwordpress -p a4lwordpress > a4lwordpress.sql
+4n1m4ls4L1f3	
 
 # Restore to Destination Database
-mysql -h CNAMEOFRDSINSTANCE -u a4lwordpress -p a4lwordpress < a4lwordpress.sql 
+mysql -h a4lwordpress1.c2pdm7e7rame.us-east-1.rds.amazonaws.com -u a4lwordpress -p a4lwordpress < a4lwordpress.sql 
 
 # Change WP Config
 cd /var/www/html
